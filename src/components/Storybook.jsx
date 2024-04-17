7f7f45274711856c3228add830a4646eaca8429c
shubham <ss691438@gmail.com>
2024-04-17 13:34:07 +0530

    routes setup

---
+++ src/components/Storybook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Storybook = () => {
+  return (
+    <div>Storybook</div>
+  )
+}
+
+export default Storybook
---
+++ src/routes.jsx
@@ -0,0 +1,30 @@
+import App from "./App"
+import { createBrowserRouter } from "react-router-dom"
+import './index.css'
+import Metrics from "./components/Metrics"
+import Logs from "./components/Logs"
+import Storybook from "./components/Storybook"
+
+const  routes=createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children:[
+        {
+          path:'metrics',
+          element:<Metrics/>
+        },
+        {
+          path:'logs',
+          element:<Logs/>
+        },
+        {
+          path:'storybook',
+          element:<Storybook/>
+        }
+      ]
+      
+    },
+])
+
+export default routes
