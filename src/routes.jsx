7f9bb36774a292339bcc6d38fbd690fba76d3d3c
shubham <ss691438@gmail.com>
2024-04-21 17:05:45 +0530

    added automatic navigation

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,32 @@ import { createBrowserRouter } from "react-router-dom"
 import './index.css'
 import Metrics from "./components/Metrics"
 import Logs from "./components/Logs"
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react"
+
+
+const Navigate = () => {
+  const navigate = useNavigate();
+  useEffect(()=>{
+    navigate('/metrics')
+  },[])
+  return (
+    <div></div>
+  )
+}
+
+
+
 
 const  routes=createBrowserRouter([
     {
       path: "/",
       element: <App/>,
       children:[
+        {
+          path:'/',
+          element:<Navigate/>
+        },
         {
           path:'metrics',
           element:<Metrics/>
